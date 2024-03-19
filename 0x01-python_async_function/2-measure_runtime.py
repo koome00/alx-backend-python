7cb57674d5ec0cd7cba1c9efddf97112c15370fe
koome <koomemc@gmail.com>
2024-03-19 23:48:47 +0300

    measure runtime

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""measure average time taken per task"""
+import asyncio
+import time
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int = 10):
+    """
+    Measure the runtime
+    """
+    start = time.perf_counter()
+    asyncio.run(wait_n(n, max_delay))
+    total_time = time.perf_counter() - start
+    return total_time / n
