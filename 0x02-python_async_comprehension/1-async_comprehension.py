cc607a016b4def0ab6b71d10f8c2335624ff10aa
koome <koomemc@gmail.com>
2024-03-20 02:21:05 +0300

    fix typo

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 """List comprehesion in async"""
-import asyncio
 from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehesion() -> List[float]:
+async def async_comprehension() -> List[float]:
     """
     List comprehesion in async
     """
