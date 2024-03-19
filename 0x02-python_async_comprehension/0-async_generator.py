33d325a9f0316d3a484c3126cdb5ba783288a77d
koome <koomemc@gmail.com>
2024-03-20 02:02:53 +0300

    fix hint type

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,15 +2,14 @@
 """async generator"""
 import asyncio
 import random
-from typing import Generator
+from typing import AsyncGenerator
 
 
-async def async_generator() -> Generator[float, None, None]:
+async def async_generator() -> AsyncGenerator[float, None]:
     """async generator"""
     i = 0
     while i < 10:
         val = random.uniform(0, 10)
-        
         await asyncio.sleep(1)
         yield val
         i += 1
