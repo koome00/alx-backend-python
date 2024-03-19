1c0d1b422a6322dd8cfd82b070c5b74559870056
koome <koomemc@gmail.com>
2024-03-20 00:08:35 +0300

    concurrent task

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""handling concurrent tasks"""
+import asyncio
+from typing import List
+
+task_wait_random = __import__('3-task').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int = 10) -> List[float]:
+    """
+    return the list of all the delays (float values)
+    in acsending order
+    """
+    delays = []
+    tasks = []
+    while n > 0:
+        task = asyncio.create_task(task_wait_random(max_delay))
+        delays.append(task)
+        n -= 1
+    for coro in asyncio.as_completed(delays):
+        tasks.append(await coro)
+    return tasks
