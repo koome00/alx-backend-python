7027583a2ad4b3b63ae8cb11536f78553d9ea58e
koome <koomemc@gmail.com>
2024-03-20 01:54:47 +0300

    change annotation to float type

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,11 +5,12 @@ import random
 from typing import Generator
 
 
-async def async_generator() -> Generator[int, None, None]:
+async def async_generator() -> Generator[float, None, None]:
     """async generator"""
     i = 0
     while i <= 10:
         val = random.uniform(0, 10)
-        yield val
+        
         await asyncio.sleep(1)
+        yield val
         i += 1
