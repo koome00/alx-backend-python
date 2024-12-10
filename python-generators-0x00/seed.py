708da808e1f0ae2a10f622b16178a7911b501366
koome <koomemc@gmail.com>
2024-12-10 17:03:27 +0300

    fix: create db, table and insert data

--- python-generators-0x00/seed.py
+++ python-generators-0x00/seed.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
+"""
+create a generator that streams rows from an SQL database one by one.
+"""
+
 from mysql.connector import connect, Error
 import os
 import csv
-
+import uuid
 
 def connect_db():
     """
     Connects to the database server
     """
     try:
-        with connect(
+        connection = connect(
             host = os.getenv("HOST"),
             password = os.getenv("PASSWORD"),
             user = os.getenv("USER")
-        ) as connection:
-            print(connection)
-            return connection
+        )
+        print(connection)
+        return connection
     except Error as e:
         print("Connect: ", e)
 
@@ -26,9 +30,9 @@ def create_database(connection):
     """
     try:
         q = "CREATE DATABASE IF NOT EXISTS ALX_prodev"
-        with connection.cursor() as cursor:
-            cursor.execute(q)
-            connection.commit()
+        cursor = connection.cursor()
+        cursor.execute(q)
+        connection.commit()
         print("Database created or already exists.")
     except Exception as e:
         print("DB: ", connection)
@@ -40,14 +44,14 @@ def connect_to_prodev():
     Connect to ALX_prodev
     """
     try:
-        with connect(
+        connection = connect(
             host = os.getenv("HOST"),
             password = os.getenv("PASSWORD"),
             user = os.getenv("USER"),
             database = "ALX_prodev"
-        ) as connection:
-            print(connection)
-            return connection
+        )
+        print("Connected to prodev: ", connection)
+        return connection
     except Error as e:
         print("Prodev: ",e)
 
@@ -56,6 +60,9 @@ def create_table(connection):
     """
     Creates a table user_data if it does not exists with the required fields
     """
+    if not connection:
+        print("No connection available")
+        return
 
     try:
         create_table_query = """
@@ -63,12 +70,13 @@ CREATE TABLE IF NOT EXISTS user_data (
     user_id BINARY(16) PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
-    age DECIMAL(5,2) NOT NULL,
+    age DECIMAL(5,2) NOT NULL
 )
 """
-        with connection.cursor() as cursor:
-            cursor.execute(create_table_query)
-            connection.commit
+        cursor = connection.cursor()
+        cursor.execute(create_table_query)
+        connection.commit()
+        print("Table exists or already created")
     except Exception as e:
         print("Table: ", e)
 
@@ -77,26 +85,20 @@ def insert_data(connection, data):
     """
     inserts data in the database if it does not exist
     """
+    
     try:
-        insert_data_query = f"""
-LOAD DATA INFILE '/{data}'
-INTO TABLE user_data
-FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
-IGNORE 1 LINES
-"""
-        with connection.cursor() as cursor:
-            cursor.execute(insert_data_query)
-            connection.commit()
+        with open(data, newline="", encoding='utf-8-sig') as csv_file:
+            reader = csv.DictReader(csv_file)
+            cursor = connection.cursor()
+
+            for row in reader:
+                user_id = uuid.uuid4().bytes
+
+                insert_query = """INSERT INTO user_data(user_id, name, email, age)
+                VALUES (%s, %s, %s, %s)
+                """
+                user_data = (user_id, row["name"], row["email"], float(row["age"]))
+                cursor.execute(insert_query, user_data)
+                connection.commit()
     except Exception as e:
-        print("Insert: ", e)
-
-
-
-connection = connect_db()
-
-if connection:
-    create_database(connection)
-    connection.close()
-else:
-    print("Failed to connect to MySQL.")
+        print("Insert Error: ", e)
