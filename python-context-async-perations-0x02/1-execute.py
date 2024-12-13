45bc88590b93412066490f664a49fdebc0324d99
koome <koomemc@gmail.com>
2024-12-13 20:35:15 +0300

    fix: use class based context manager

--- python-context-async-perations-0x02/1-execute.py
+++ python-context-async-perations-0x02/1-execute.py
@@ -1,29 +1,34 @@
 #!/usr/bin/python3
 
 import sqlite3
-from contextlib import contextmanager
 
-@contextmanager
-def ExecuteQuery(db_path, query, parameter):
-    conn = sqlite3.connect(db_path)
-    cursor = conn.cursor()
-    try:
-        cursor.execute(query, (parameter,))
-        results = cursor.fetchall()
-        yield results
-        conn.commit()
-    except:
-        conn.rollback()
-        raise
-    finally:
-        conn.close()
+
+class ExecuteQuery:
+    def __init__(self, db_path, query, parameters):
+        self.db_path = db_path
+        self.query = query
+        self.parameters = parameters
+
+    def __enter__(self):
+        self.conn = sqlite3.connect(self.db_path)
+        self.cursor = self.conn.cursor()
+        self.cursor.execute(self.query, self.parameters)
+        self.results = self.cursor.fetchall()
+        return self.results
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_type is None:
+            self.conn.commit()
+        else:
+            self.conn.rollback()
+        self.conn.close()
 
 if __name__ == "__main__":
     db_path = 'users.db'
     query = "SELECT * FROM users WHERE age > ?"
-    parameter = 25
+    parameter = (25,)
 
     with ExecuteQuery(db_path, query, parameter) as results:
-        print("Query Results:")
         for row in results:
             print(row)
+
