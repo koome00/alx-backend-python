dc04788eb7d56d34e87e1271024c32aadb2b895c
koome <koomemc@gmail.com>
2024-12-13 19:55:38 +0300

    feat: log queries

---
+++ python-decorators-0x01/0-log_queries.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import sqlite3
+import functools
+
+#### decorator to lof SQL queries
+
+def log_queries(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        query = kwargs.get('query') if 'query' else args[0]
+        print(query)
+        return func(*args, **kwargs)
+    return wrapper
+        
+
+@log_queries
+def fetch_all_users(query):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    conn.close()
+    return results
+
+#### fetch users while logging the query
+users = fetch_all_users(query="SELECT * FROM users")
