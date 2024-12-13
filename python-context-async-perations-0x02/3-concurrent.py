db962c839f54a9fdc0401b8a47213c7956ba777a
koome <koomemc@gmail.com>
2024-12-13 20:49:12 +0300

    fix: remove func parameters

--- python-context-async-perations-0x02/3-concurrent.py
+++ python-context-async-perations-0x02/3-concurrent.py
@@ -3,19 +3,21 @@
 import asyncio
 import aiosqlite
 
-async def async_fetch_users(db_path):
+async def async_fetch_users():
+    db_path = 'users.db'
     async with aiosqlite.connect(db_path) as db:
         async with db.execute("SELECT * FROM users") as cursor:
             return await cursor.fetchall()
 
-async def async_fetch_older_users(db_path, age):
+async def async_fetch_older_users():
+    db_path = 'users.db'
     async with aiosqlite.connect(db_path) as db:
-        async with db.execute("SELECT * FROM users WHERE age > ?", (age,)) as cursor:
+        async with db.execute("SELECT * FROM users WHERE age > ?", (40,)) as cursor:
             return await cursor.fetchall()
 
 async def fetch_concurrently():
     db_path = 'users.db'
-    users, older_users = await asyncio.gather(async_fetch_users(db_path), async_fetch_older_users(db_path, 40))
+    users, older_users = await asyncio.gather(async_fetch_users(), async_fetch_older_users())
     
     print("All Users:")
     for user in users:
